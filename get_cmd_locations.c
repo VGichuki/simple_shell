ff32ba10e6af990e2355f41da511b61ff7636469
Jess <jessgatura@gmail.com>
2023-06-20 00:03:40 +0300

    added function to get the full path of a command

---
+++ get_cmd_locations.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * location - returns full path of a command
+ * @cmd: the command
+ *
+ * Return: pointer to the full path
+ */
+
+char *location(const char *cmd)
+{
+	char *path, *token, *full_path;
+	char delim = ":";
+	int len;
+
+	path = getenv("PATH");
+	if (!path)
+	{
+		return (NULL);
+	}
+
+	token = strtok(path, delim);
+
+	while (token)
+	{
+		len = strlen(token) + strlen(cmd) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			free(full_path);
+			return (NULL);
+		}
+		sprintf(full_path, "%s/%s", token, cmd);
+		if (access(full_path, F_OK) == 0)
+			return (full_path);
+		free(full_path);
+		token = strtok(NULL, delim);
+	}
+	return (NULL);
+}
