2174d32c2e4b2738bccb2722be03ae204225d809
wangari <wangarigichukis@gmail.com>
2023-06-21 19:43:10 -0700

    add new functions to shell.h file

--- shell.h
+++ shell.h
@@ -15,11 +15,13 @@ extern char **environ
 
 /*Function declarations*/
 int exec(char **av);
+void print_env(void);
 int bettyStyle(int a, int b);
 int _putchar(char c);
 char *location(const char *cmd);
 char **tokenize(char *str);
 void prompt(void);
 int main(int argc, char **argv);
+
 
 #endif
