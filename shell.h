fd8583e5a08d7403b739af11dd4df5d5ea9153d1
wangari <wangarigichukis@gmail.com>
2023-06-21 19:40:51 -0700

    add new functions to shell.h file

--- shell.h
+++ shell.h
@@ -6,17 +6,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 #define BUFF_SIZE 1024
 
 /*Global environment*/
 extern char **environ
 
-/*function declarations*/
+/*Function declarations*/
 int exec(char **av);
 int bettyStyle(int a, int b);
 int _putchar(char c);
 char *location(const char *cmd);
-
+char **tokenize(char *str);
+void prompt(void);
+int main(int argc, char **argv);
 
 #endif
